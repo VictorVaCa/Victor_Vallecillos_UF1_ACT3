535798d0c7f5c6f81d3f867c62e8c2821137352e
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2024-10-12 19:00:14 +0200

    Commit Inicial

---
+++ app/src/main/java/com/example/victor_vallecillos_uf1_act3/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.victor_vallecillos_uf1_act3
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textViewHola = findViewById<TextView>(R.id.textViewHola)
+        val textViewMon = findViewById<TextView>(R.id.textViewMon)
+    }
+}
