24c910ea263435a0ae36672a484a26cbe4c29979
VictorVaCa <a17vicvalcam@inspedralbes.cat>
2024-10-13 13:51:28 +0200

    Practica finalitzada

--- app/src/main/java/com/example/victor_vallecillos_uf1_act3/MainActivity.kt
+++ app/src/main/java/com/example/victor_vallecillos_uf1_act3/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.victor_vallecillos_uf1_act3
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,13 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val textViewHola: TextView = findViewById(R.id.textViewHola)
-        val textViewMon: TextView = findViewById(R.id.textViewMon)
-
-        textViewHola.text = getString(R.string.hola)
-        textViewMon.text = getString(R.string.mon)
-
-        textViewHola.setTextColor(ContextCompat.getColor(this, R.color.red))
-        textViewMon.setTextColor(ContextCompat.getColor(this, R.color.blue))
     }
 }
